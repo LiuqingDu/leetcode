936d03b048c93cda408ce1a60de5ea4c196a11c9
duliuqing <me@duliuqing.com>
2023-07-24 20:52:59 -0700

    13

--- 13.罗马数字转整数.java
+++ 13.罗马数字转整数.java
@@ -18,16 +18,25 @@ class Solution {
     value.put('D', 500);
     value.put('M', 1000);
 
+    // 罗马数字的规律是，如果一个数字比右边小，则减去当前数字
+    // 如果当前数字比右边大或者相等，则加上这个数字
+    // 可以把当前数字与右边比较，来判断是加还是减
+    // 最后一位没有再右边的数字，永远是加
+
     int result = 0;
-    int n = s.length();
-    for (int i = 0; i < (n - 1); i++) {
-      if (value.get(s.charAt(i)) < value.get(s.charAt(i + 1))) {
-        result -= value.get(s.charAt(i));
-      } else {
+    for (int i = 0; i < s.length(); i++) {
+      // 最后一位直接加
+      if (i == s.length() - 1) {
         result += value.get(s.charAt(i));
+      } else {
+        // 与下一位比较，相等或者大于都是加
+        if (value.get(s.charAt(i)) >= value.get(s.charAt(i + 1))) {
+          result += value.get(s.charAt(i));
+        } else {
+          result -= value.get(s.charAt(i));
+        }
       }
     }
-    result += value.get(s.charAt(n - 1));
     return result;
   }
 }
