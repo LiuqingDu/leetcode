882aa9ba1b78af5d519114086fe3191790492587
LiuqingDu <me@duliuqing.com>
2023-07-27 16:34:12 -0700

    141

---
+++ 141.环形链表.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=java
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // 快慢指针，相遇了则有环
+        // 要判断当前节点和下一个节点不为空，因为要取next和next.next 
+        // 当前不为空可取到next，next不为空可取到next.next
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
