743532ddb33a59068f00b6e79f65f7b25dd0c784
LiuqingDu <me@duliuqing.com>
2023-07-28 15:22:51 -0700

    191

---
+++ 191.位-1-的个数.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=191 lang=java
+ *
+ * [191] 位1的个数
+ */
+
+// @lc code=start
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int res = 0;
+
+        // n&(n-1) 可以消除二进制最后一个1
+        while(n != 0) {
+            n = n & (n - 1);
+            res ++;
+        }
+        return res;
+    }
+}
+// @lc code=end
+
