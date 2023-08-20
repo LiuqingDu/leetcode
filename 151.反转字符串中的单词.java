02833e1eaee3c0e01ffdac46d1065858f993935e
LiuqingDu <me@duliuqing.com>
2023-08-19 23:58:32 -0700

    151 update comment

--- 151.反转字符串中的单词.java
+++ 151.反转字符串中的单词.java
@@ -7,14 +7,16 @@
 // @lc code=start
 class Solution {
     public String reverseWords(String s) {
+        // 思路是先把整个字符串翻转，然后再把每一个单词翻转，就实现了把原单词顺序翻转
         StringBuilder sb = new StringBuilder();
         // 把多于的空格都删掉
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
+            // 不是空格的话就直接加到新字符串里
             if (c != ' ') {
-                // 单词中的字母/数字
                 sb.append(c);
             } else if (!sb.isEmpty() && sb.charAt(sb.length() - 1) != ' ') {
+                // 是空格，且新字符串最后一个不是空格，则把这个空格加进去，这样可以避免加入多余的空格
                 // 单词之间保留一个空格
                 sb.append(' ');
             }
@@ -22,12 +24,13 @@ class Solution {
         if (sb.isEmpty()) {
             return "";
         }
-        // 末尾如果有空格，删掉
+
+        // 末尾有可能有一个多余的空格
         if (sb.charAt(sb.length() - 1) == ' ') {
             sb.deleteCharAt(sb.length() - 1);
         }
 
-        // 清洗之后的字符串
+        // 去除多余空格的字符串
         char[] chars = sb.toString().toCharArray();
         int n = chars.length;
         // 进行单词的翻转，先整体翻转
@@ -35,7 +38,7 @@ class Solution {
         // 再把每个单词翻转
         for (int i = 0; i < n;) {
             for (int j = i; j < n; j++) {
-                if (j + 1 == n || chars[j + 1] == '') {
+                if (j + 1 == n || chars[j + 1] == ' ') {
                     reverse(chars, i , j);
                     i = j + 2;
                     break;
@@ -47,6 +50,7 @@ class Solution {
 
     }
 
+    // 翻转字符串
     private void reverse(char[] arr, int i, int j) {
         while (i < j) {
             char tmp = arr[i];
