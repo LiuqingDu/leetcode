05d89c14ea1205122ac2784ef24d18febd2e8ec7
LiuqingDu <me@duliuqing.com>
2023-08-26 23:49:34 -0700

    701

---
+++ 701.二叉搜索树中的插入操作.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=701 lang=java
+ *
+ * [701] 二叉搜索树中的插入操作
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // 二叉搜索树的节点是有序的，题目保证数值独一无二，则对于给定的一个数，
+    // 肯定可以找到一个叶子节点刚好略大于或者略小于val，那么把val放在这个叶子节点的左右侧即可
+    // 这个左右侧原本是空节点，找到这个空节点，返回一个新的节点即可
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        }
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
+// @lc code=end
+
