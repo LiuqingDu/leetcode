e664235c8d4faee40e17f142f44e6d67a09d31b7
Liuqing Du <me@duliuqing.com>
2023-10-13 23:03:59 -0700

    14

---
+++ 14.最长公共前缀.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=javascript
+ *
+ * [14] 最长公共前缀
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            if (i >= strs[j].length || strs[0].charAt(i) !== strs[j].charAt(i)) {
+                return strs[0].substring(0, i);
+            }
+        }
+    }
+    return strs[0]
+
+};
+// @lc code=end
+
