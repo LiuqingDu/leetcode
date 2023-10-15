fde6a4f5f84f0cc71397ff7881aa0b9117e01a9c
Liuqing Du <me@duliuqing.com>
2023-10-14 19:27:41 -0700

    21

---
+++ 21.合并两个有序链表.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=javascript
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+
+    let dump = new ListNode();
+    let head = dump;
+    let l1 = list1;
+    let l2 = list2;
+    while(l1 && l2) {
+        if (l1.val <= l2.val) {
+            head.next = l1;
+            l1 = l1.next;
+        } else {
+            head.next = l2;
+            l2 = l2.next;
+        }
+        head = head.next;
+    }
+    if (l1) {
+        head.next = l1;
+    }
+    if (l2) {
+        head.next = l2;
+    }
+    return dump.next;
+
+};
+// @lc code=end
+
