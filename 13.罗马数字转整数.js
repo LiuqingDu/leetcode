3f6aa2bb1296e666bce9082c27a6401fd2323410
Liuqing Du <me@duliuqing.com>
2023-10-15 21:27:22 -0700

    13

---
+++ 13.罗马数字转整数.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=13 lang=javascript
+ *
+ * [13] 罗马数字转整数
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+
+    let map = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+
+    let res = 0;
+
+    for (let i = 0; i < s.length - 1; i++) {
+        if (map[s.charAt(i)] >= map[s.charAt(i + 1)]) {
+            res += map[s.charAt(i)];
+        } else {
+            res -= map[s.charAt(i)];
+        }
+    }
+    res += map[s.charAt(s.length - 1)];
+
+    return res;
+
+};
+// @lc code=end
+
