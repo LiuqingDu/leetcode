1d75ca4da22e2e45fe5af42486cf5cf67196c998
Liuqing Du <me@duliuqing.com>
2023-10-22 22:45:21 -0700

    83

---
+++ 83.删除排序链表中的重复元素.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=javascript
+ *
+ * [83] 删除排序链表中的重复元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+
+    if (!head) {
+        return head;
+    }
+
+    let slow = head, fast = head.next;
+
+    while(fast) {
+        if(slow.val === fast.val) {
+            slow.next = null;
+            fast = fast.next;
+        } else {
+            slow.next = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+    }
+
+    return head;
+
+};
+// @lc code=end
+
