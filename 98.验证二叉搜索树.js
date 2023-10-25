6b8eb822c25de7edfb9d9416ee64b21b3697c67d
Liuqing Du <me@duliuqing.com>
2023-10-24 23:47:52 -0700

    98

---
+++ 98.验证二叉搜索树.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=javascript
+ *
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+
+    let isValid = function(node, min, max) {
+        if (!node) {
+            return true;
+        }
+        if (min != null && node.val <= min) {
+            return false;
+        }
+        if (max != null && node.val >= max) {
+            return false;
+        }
+        return isValid(node.left, min, node.val) && isValid(node.right, node.val, max);
+
+    }
+
+    return isValid(root, null, null);
+
+};
+// @lc code=end
+
