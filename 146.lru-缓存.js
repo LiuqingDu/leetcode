8eb60b0615ea7c17aff5cb3000dc02d3c9c92e07
Liuqing Du <me@duliuqing.com>
2023-11-05 20:00:38 -0700

    146

---
+++ 146.lru-缓存.js
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=146 lang=javascript
+ *
+ * [146] LRU 缓存
+ */
+
+// @lc code=start
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    this.cap = capacity;
+    this.map = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if (!this.map.has(key)) {
+        return -1;
+    }
+    let val = this.map.get(key);
+    this.map.delete(key);
+    this.map.set(key, val);
+    return val;
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    if (this.map.has(key)) {
+        this.map.delete(key);
+    } else {
+        if (this.map.size >= this.cap) {
+            this.map.delete(this.map.keys().next().value);
+        }
+    }
+    this.map.set(key, value);
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+// @lc code=end
+
