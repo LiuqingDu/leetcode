2aa3bf13e8d56a10220609356878df2a8bfbec5f
Liuqing Du <me@duliuqing.com>
2023-12-01 20:42:05 -0700

    300

---
+++ 300.最长递增子序列.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=javascript
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+
+    let n = nums.length;
+    let dp = new Array(n).fill(1);
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    let res = 1;
+    for (let i = 1; i < n; i++) {
+        res = Math.max(res, dp[i]);
+    }
+
+    return res;
+
+};
+// @lc code=end
+
