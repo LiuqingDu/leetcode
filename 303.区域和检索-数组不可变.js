d53ac5497cc00cf3fab492ec6d39f1a29939c8ce
Liuqing Du <me@duliuqing.com>
2023-12-02 20:46:10 -0700

    303

---
+++ 303.区域和检索-数组不可变.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=javascript
+ *
+ * [303] 区域和检索 - 数组不可变
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+
+    this.sums = [];
+    this.nums = nums;
+
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        this.sums[i] = sum;
+    }
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(left, right) {
+
+    return this.sums[right] - this.sums[left] + this.nums[left];
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
+// @lc code=end
+
