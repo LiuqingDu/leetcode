5b77491f63a63fafd25ac67e338ccb0ca051a7b0
Liuqing Du <me@duliuqing.com>
2023-12-24 23:14:57 -0700

    583

---
+++ 583.两个字符串的删除操作.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=583 lang=javascript
+ *
+ * [583] 两个字符串的删除操作
+ */
+
+// @lc code=start
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function(word1, word2) {
+
+    let m = word1.length;
+    let n = word2.length;
+
+    let dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (word1.charAt(i - 1) === word2.charAt(j - 1)) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+    }
+
+    let l = dp[m][n];
+
+    return (m - l) + (n - l);
+
+};
+// @lc code=end
+
