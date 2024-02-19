781598eb1e56433d8c1896d7066c2c763f12d668
Liuqing Du <me@duliuqing.com>
2024-02-18 22:56:11 -0700

    41

--- 41.缺失的第一个正数.java
+++ 41.缺失的第一个正数.java
@@ -12,9 +12,17 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
-
+                swap(nums, i, nums[i] - 1);
             }
         }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] - 1 != i) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
 
     }
 
