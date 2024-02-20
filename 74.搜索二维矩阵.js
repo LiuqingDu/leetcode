45d37c46ca9401798914949fe02b0ff798e866ae
Liuqing Du <me@duliuqing.com>
2024-02-19 21:53:59 -0700

    74

--- 74.搜索二维矩阵.js
+++ 74.搜索二维矩阵.js
@@ -12,6 +12,27 @@
  */
 var searchMatrix = function(matrix, target) {
 
+    let m = matrix.length;
+    let n = matrix[0].length;
+
+    let l = 0;
+    let r = m * n - 1;
+
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2);
+        let row = Math.floor(mid / n);
+        let col = mid % n;
+        if (matrix[row][col] < target) {
+            l = mid + 1;
+        } else if (matrix[row][col] > target) {
+            r = mid - 1;
+        } else if (matrix[row][col] === target) {
+            return true;
+        }
+    }
+
+    return false;
+
 };
 // @lc code=end
 
