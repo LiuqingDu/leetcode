59b385e0d4cfd0f03a13f35520e5990be337407a
Liuqing Du <me@duliuqing.com>
2024-02-22 20:19:57 -0700

    80 update comment

--- 80.删除有序数组中的重复项-ii.js
+++ 80.删除有序数组中的重复项-ii.js
@@ -19,20 +19,25 @@ var removeDuplicates = function(nums) {
     let n = nums.length;
 
     while (fast < n) {
+        // 如果相等
         if (nums[fast] === nums[slow]) {
+            // 看慢指针已经存放了多少个相同元素，如果还没有放够两个，则可以继续放
             if (c < 2) {
+                // slow指向的是已经存放好的末尾，因此下一个位置需要+1
                 slow++;
                 nums[slow] = nums[fast];
                 fast++;
                 c++;
             } else {
+                // 如果已经放了2个，则只挪动快指针
                 fast++;
             }
         } else {
-            c = 1;
+            // 如果有一个新的元素，则直接放进去，且记录c=1
             slow++;
             nums[slow] = nums[fast];
             fast++;
+            c = 1;
         }
     }
 
