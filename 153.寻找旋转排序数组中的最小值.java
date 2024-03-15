7ecf453e3ad54a3fe749db3894bb03f43a502192
Liuqing Du <me@duliuqing.com>
2024-03-14 20:38:03 -0700

    153

---
+++ 153.寻找旋转排序数组中的最小值.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=153 lang=java
+ *
+ * [153] 寻找旋转排序数组中的最小值
+ */
+
+// @lc code=start
+class Solution {
+    public int findMin(int[] nums) {
+
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+
+    }
+}
+// @lc code=end
+
