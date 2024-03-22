30b5b6b673adde0115335c70ddfec3a2faecc7b9
Liuqing Du <me@duliuqing.com>
2024-03-21 21:22:08 -0700

    189

---
+++ 189.轮转数组.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=java
+ *
+ * [189] 轮转数组
+ */
+
+// @lc code=start
+class Solution {
+    public void rotate(int[] nums, int k) {
+
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+}
+// @lc code=end
+
