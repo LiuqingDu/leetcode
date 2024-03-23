b2ed3e96543503a9cd016a0ae8a16e553c636d46
Liuqing Du <me@duliuqing.com>
2024-03-22 21:27:32 -0700

    208

---
+++ 208.实现-trie-前缀树.java
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=208 lang=java
+ *
+ * [208] 实现 Trie (前缀树)
+ */
+
+// @lc code=start
+class Trie {
+    boolean end;
+    Trie[] children;
+
+    public Trie() {
+
+        this.end = false;
+        this.children = new Trie[26];
+    }
+    
+    public void insert(String word) {
+
+        Trie node = this;
+        for (char c: word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new Trie();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.end = true;
+    }
+    
+    public boolean search(String word) {
+
+        Trie node = this;
+        for (char c: word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return node.end;
+    }
+    
+    public boolean startsWith(String prefix) {
+
+        Trie node = this;
+        for (char c: prefix.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+// @lc code=end
+
