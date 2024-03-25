cc41a2fc8368c49b74ea39db4aecc81bfc34b0f7
Liuqing Du <me@duliuqing.com>
2024-03-24 22:59:56 -0700

    211

---
+++ 211.添加与搜索单词-数据结构设计.java
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=211 lang=java
+ *
+ * [211] 添加与搜索单词 - 数据结构设计
+ */
+
+// @lc code=start
+class WordDictionary {
+    
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean end = false;
+    }
+
+    TrieNode root = new TrieNode();
+
+    public WordDictionary() {
+
+    }
+    
+    public void addWord(String word) {
+
+        TrieNode node = root;
+        for (char c: word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.end = true;
+
+    }
+    
+    public boolean search(String word) {
+
+        return dfs(root, word.toCharArray(), 0);
+    }
+
+    private boolean dfs(TrieNode node, char[] chars, int ind) {
+        if (ind == chars.length) {
+            return node.end;
+        }
+        if (node == null) {
+            return false;
+        }
+        if (chars[ind] != '.') {
+            return node.children[chars[ind] - 'a'] != null && dfs(node.children[chars[ind] - 'a'], chars, ind + 1);
+        } else {
+            for (TrieNode t: node.children) {
+                if (t != null && dfs(t, chars, ind + 1)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
+// @lc code=end
+
