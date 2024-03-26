d508ff303a7ebc07bc289ebde32cd3b2433c44bf
Liuqing Du <me@duliuqing.com>
2024-03-26 15:05:49 -0700

    78

---
+++ 221.最大正方形.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=221 lang=java
+ *
+ * [221] 最大正方形
+ */
+
+// @lc code=start
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] dp = new int[m][n];
+        int res = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '0') {
+                    dp[i][j] = 0;
+                } else {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                }
+                res = Math.max(res, dp[i][j]);
+            }
+        }
+
+        return res * res;
+    }
+}
+// @lc code=end
+
