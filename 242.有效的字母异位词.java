f4ac68f7032fe31bd360bea46068a9a132a28eff
Liuqing Du <me@duliuqing.com>
2024-03-30 21:47:49 -0700

    242

---
+++ 242.有效的字母异位词.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=242 lang=java
+ *
+ * [242] 有效的字母异位词
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        int n = s.length();
+        int[] arr = new int[26];
+        
+        for (int i = 0; i < n; i++) {
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+// @lc code=end
+
