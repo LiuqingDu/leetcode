01ec1dc99611ebb879417467238ff466ee1a93ca
Liuqing Du <me@duliuqing.com>
2024-05-01 23:39:44 -0500

    69

--- 69.x-的平方根.js
+++ 69.x-的平方根.js
@@ -17,14 +17,16 @@ var mySqrt = function(x) {
 
     let left = 1;
     let right = Math.floor(x / 2);
-    while (left < right) {
-        let mid = Math.floor(left + (right - left + 1) / 2);
-        if (mid > Math.floor(x / mid)) {
-            right = mid - 1;
-        } else if (mid == Math.floor(x / mid)) {
+
+    while (left <= right) {
+        let mid = Math.floor(left + (right - left) / 2);
+        
+        if (mid * mid <= x && (mid + 1) * (mid + 1) > x) {
             return mid;
-        } else if (mid < Math.floor(x / mid)) {
-            left = mid;
+        } else if (mid * mid > x) {
+            right = mid - 1;
+        } else if (mid * mid < x) {
+            left = mid + 1;
         }
     }
     
