e39e6e48a120c6a9f35ee998d334f0056e1f4e57
Liuqing Du <me@duliuqing.com>
2024-05-16 21:32:38 -0500

    203

---
+++ 203.移除链表元素.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=java
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummp = new ListNode();
+        dummp.next = head;
+        head = dummp;
+        while (head.next != null) {
+            if (head.next.val == val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+        return dummp.next;
+    }
+}
+// @lc code=end
+
