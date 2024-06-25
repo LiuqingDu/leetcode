8e26099c6b0624c741012c366a23b44bcc1ecd76
Liuqing Du <me@duliuqing.com>
2024-06-24 22:14:40 -0500

    506

---
+++ 506.相对名次.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=506 lang=javascript
+ *
+ * [506] 相对名次
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ */
+var findRelativeRanks = function(score) {
+    let res = [];
+    let ss = ["Gold Medal","Silver Medal","Bronze Medal"];
+    let copy = [...score];
+    let map = new Map();
+    copy.sort((a, b) => a - b);
+    for (let i = 0; i < copy.length; i++) {
+        map.set(copy[i], copy.length - 1 - i)
+    }
+    for (let c of score) {
+        res.push(map.get(c) < 3 ? ss[map.get(c)] : map.get(c) + 1 + "");
+    }
+    return res;
+
+};
+// @lc code=end
+
