22fa7e9f9c1f9edb4385b5c2dcb3c6a83ac559d3
Liuqing Du <me@duliuqing.com>
2024-07-23 22:55:55 -0500

    680

---
+++ 680.验证回文串-ii.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=680 lang=java
+ *
+ * [680] 验证回文串 II
+ */
+
+// @lc code=start
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+// @lc code=end
+
