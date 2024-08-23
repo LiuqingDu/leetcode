2c5f3978769463ec6798b8c24b316573a6869020
Liuqing Du <me@duliuqing.com>
2024-08-22 20:43:20 -0500

    860

---
+++ 860.柠檬水找零.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=860 lang=java
+ *
+ * [860] 柠檬水找零
+ */
+
+// @lc code=start
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        for (int i = 0; i < bills.length; i++) {
+            if (bills[i] == 5) {
+                five++;
+            } else if (bills[i] == 10) {
+                if (five >= 1) {
+                    five--;
+                    ten++;
+                } else {
+                    return false;
+                }
+            } else if (bills[i] == 20) {
+                if (ten >= 1 && five >= 1) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
+
