d602fbeabf20e9df4d5326d2ae56fae72927c300
Liuqing Du <me@duliuqing.com>
2024-08-23 17:19:22 -0500

    867

---
+++ 867.转置矩阵.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=867 lang=javascript
+ *
+ * [867] 转置矩阵
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+var transpose = function(matrix) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+
+    let res = Array.from(new Array(n), () => new Array(m));
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            res[j][i] = matrix[i][j];
+        }
+    }
+
+    return res;
+};
+// @lc code=end
+
