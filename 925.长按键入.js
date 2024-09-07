192fe50b4630765d4f1d8d4a2fc48df901814f22
Liuqing Du <me@duliuqing.com>
2024-09-06 21:53:36 -0500

    925

---
+++ 925.长按键入.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=925 lang=javascript
+ *
+ * [925] 长按键入
+ */
+
+// @lc code=start
+/**
+ * @param {string} name
+ * @param {string} typed
+ * @return {boolean}
+ */
+var isLongPressedName = function(name, typed) {
+    let n = name.length;
+    let m = typed.length;
+    let i = 0, j = 0;
+
+    while (j < m) {
+        if (i < n && name[i] === typed[j]) {
+            i++;
+            j++;
+        } else if (j > 0 && typed[j] === typed[j - 1]) {
+            j++;
+        } else {
+            return false;
+        }
+    }
+    return i === n;
+
+};
+// @lc code=end
+
