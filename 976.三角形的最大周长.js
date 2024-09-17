d059adc58d14359daaac7fe55bb2b4445b583937
Liuqing Du <me@duliuqing.com>
2024-09-16 23:08:22 -0500

    976

---
+++ 976.三角形的最大周长.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=976 lang=javascript
+ *
+ * [976] 三角形的最大周长
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function(nums) {
+    nums.sort((a, b) => a - b);
+    for (let i = nums.length - 1; i >= 2; i--) {
+        if (nums[i - 2] + nums[i - 1] > nums[i]) {
+            return nums[i - 2] + nums[i - 1] + nums[i];
+        }
+    }
+    return 0;
+};
+// @lc code=end
+
