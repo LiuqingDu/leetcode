4cf9b657030f27d66d9cb67959ca4c8bf5c74eea
Liuqing Du <me@duliuqing.com>
2024-09-18 22:17:20 -0500

    33

---
+++ 33.搜索旋转排序数组.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=javascript
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let l = 0, r = nums.length - 1;
+
+    while (l <= r) {
+        let mid = l + Math.floor((r - l) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] >= nums[l]) {
+            if (target >= nums[l] && target < nums[mid]) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        } else if (nums[mid] < nums[l]) {
+            if (target > nums[mid] && target <= nums[r]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+};
+// @lc code=end
+
