debf68774e078dfc49ca022e279ffe3a9f8d6e30
Liuqing Du <me@duliuqing.com>
2024-09-24 22:39:58 -0500

    89

---
+++ 89.格雷编码.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=89 lang=javascript
+ *
+ * [89] 格雷编码
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var grayCode = function(n) {
+    let res = [0];
+    let head = 1;
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = res.length - 1; j >= 0; j--) {
+            res.push(head + res[j])
+        }
+        head <<= 1;
+    }
+
+    return res;
+};
+// @lc code=end
+
