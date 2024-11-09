f83ac8582dd5c488b6cf8a7d6f24f4a8499605c0
Liuqing Du <me@duliuqing.com>
2024-11-08 21:25:35 -0600

    451

---
+++ 451.根据字符出现频率排序.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=451 lang=javascript
+ *
+ * [451] 根据字符出现频率排序
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function(s) {
+    const map = new Map();
+    const length = s.length;
+    for (let i = 0; i < length; i++) {
+        const c = s[i];
+        const frequency = (map.get(c) || 0) + 1;
+        map.set(c, frequency);
+    }
+    const list = [...map.keys()];
+    list.sort((a, b) => map.get(b) - map.get(a));
+    const sb = [];
+    const size = list.length;
+    for (let i = 0; i < size; i++) {
+        const c = list[i];
+        const frequency = map.get(c);
+        for (let j = 0; j < frequency; j++) {
+            sb.push(c);
+        }
+    }
+    return sb.join('');
+};
+// @lc code=end
+
