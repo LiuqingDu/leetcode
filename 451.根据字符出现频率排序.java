b3d2d7120526aaaa601e6a9e74c7a0284e21a171
Liuqing Du <me@duliuqing.com>
2024-11-08 21:25:03 -0600

    451

---
+++ 451.根据字符出现频率排序.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=451 lang=java
+ *
+ * [451] 根据字符出现频率排序
+ */
+
+// @lc code=start
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            int frequency = map.getOrDefault(c, 0) + 1;
+            map.put(c, frequency);
+        }
+        List<Character> list = new ArrayList<Character>(map.keySet());
+        Collections.sort(list, (a, b) -> map.get(b) - map.get(a));
+        StringBuffer sb = new StringBuffer();
+        int size = list.size();
+        for (int i = 0; i < size; i++) {
+            char c = list.get(i);
+            int frequency = map.get(c);
+            for (int j = 0; j < frequency; j++) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
+// @lc code=end
+
