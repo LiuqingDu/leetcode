5a26bde330669832602539dcf7dcf3465934b425
Liuqing Du <me@duliuqing.com>
2025-01-03 22:10:39 -0600

    714

---
+++ 714.买卖股票的最佳时机含手续费.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=714 lang=javascript
+ *
+ * [714] 买卖股票的最佳时机含手续费
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @param {number} fee
+ * @return {number}
+ */
+var maxProfit = function(prices, fee) {
+    let n = prices.length;
+    let dp = Array.from(new Array(n), () => new Array(2).fill(0));
+
+    dp[0][1] = - prices[0];
+    for (let i = 1; i < n; i++) {
+        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+        dp[i][1] = Math.max(dp[i - 1][0] - prices[i], dp[i - 1][1]);
+    }
+
+    return dp[n - 1][0];
+};
+// @lc code=end
+
