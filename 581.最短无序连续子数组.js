c95aa00508a8ab58986c23cdcee41219c766fcf0
Liuqing Du <me@duliuqing.com>
2025-01-14 22:10:55 -0600

    581

---
+++ 581.最短无序连续子数组.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=581 lang=javascript
+ *
+ * [581] 最短无序连续子数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findUnsortedSubarray = function(nums) {
+    const n = nums.length;
+    let maxn = -Number.MAX_VALUE, right = -1;
+    let minn = Number.MAX_VALUE, left = -1;
+    for (let i = 0; i < n; i++) {
+        if (maxn > nums[i]) {
+            right = i;
+        } else {
+            maxn = nums[i];
+        }
+        if (minn < nums[n - i - 1]) {
+            left = n - i - 1;
+        } else {
+            minn = nums[n - i - 1];
+        }
+    }
+    return right === -1 ? 0 : right - left + 1;
+};
+// @lc code=end
+
