3dbc1d97eb3680d45bb690d767f647b73a8e278e
Liuqing Du <me@duliuqing.com>
2025-02-05 20:19:59 -0600

    1103

---
+++ 1103.分糖果-ii.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=1103 lang=java
+ *
+ * [1103] 分糖果 II
+ */
+
+// @lc code=start
+class Solution {
+    public int[] distributeCandies(int candies, int num_people) {
+        // 直接按题意模拟
+        int[] ans = new int[num_people];
+        int i = 0;
+        while (candies != 0) {
+            ans[i % num_people] += Math.min(candies, i + 1);
+            candies -= Math.min(candies, i + 1);
+            i += 1;
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
