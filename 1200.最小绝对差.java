5988f79d74cdb2f82f35d4d45a7584593348b0a8
Liuqing Du <me@duliuqing.com>
2025-02-18 21:26:56 -0600

    1200

---
+++ 1200.最小绝对差.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=1200 lang=java
+ *
+ * [1200] 最小绝对差
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        // 先排序,那么最小差就出现在相邻的两个数之间
+        // 如果等于当前最小差则当前两数字加入结果
+        // 如果小,则更新最小差并重置结果
+
+        int n = arr.length;
+        Arrays.sort(arr);
+
+        int best = Integer.MAX_VALUE;
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        for (int i = 0; i < n - 1; ++i) {
+            int delta = arr[i + 1] - arr[i];
+            if (delta < best) {
+                best = delta;
+                ans.clear();
+                List<Integer> pair = new ArrayList<Integer>();
+                pair.add(arr[i]);
+                pair.add(arr[i + 1]);
+                ans.add(pair);
+            } else if (delta == best) {
+                List<Integer> pair = new ArrayList<Integer>();
+                pair.add(arr[i]);
+                pair.add(arr[i + 1]);
+                ans.add(pair);
+            }
+        }
+
+        return ans;
+    }
+}
+// @lc code=end
+
