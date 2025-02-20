c82b6ad0895ef7f8b3a622e347786bd56617c82d
Liuqing Du <me@duliuqing.com>
2025-02-19 22:11:34 -0600

    1207

---
+++ 1207.独一无二的出现次数.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=1207 lang=java
+ *
+ * [1207] 独一无二的出现次数
+ */
+
+// @lc code=start
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        // 先用一个 map 统计每个数出现次数
+        // 再用另一个 set 统计看是否存在同样的次数
+
+        Map<Integer, Integer> occur = new HashMap<Integer, Integer>();
+        for (int x : arr) {
+            occur.put(x, occur.getOrDefault(x, 0) + 1);
+        }
+        Set<Integer> times = new HashSet<Integer>();
+        for (Map.Entry<Integer, Integer> x : occur.entrySet()) {
+            times.add(x.getValue());
+        }
+        return times.size() == occur.size();
+    }
+}
+// @lc code=end
+
