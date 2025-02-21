11c142520d9d18b4d4bbe8fad6fb25d646c5fb58
Liuqing Du <me@duliuqing.com>
2025-02-20 23:07:26 -0600

    767

---
+++ 767.重构字符串.js
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=767 lang=javascript
+ *
+ * [767] 重构字符串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reorganizeString = function(s) {
+    const getIdx = (c) => c.charCodeAt() - 'a'.charCodeAt();
+    const getAlpha = (c) => String.fromCharCode(c);
+    if (s.length < 2) {
+        return s;
+    }
+    const counts = new Array(26).fill(0);
+    let maxCount = 0;
+    const length = s.length;
+    for (let i = 0; i < length; i++) {
+        const c = s.charAt(i);
+        counts[getIdx(c)]++;
+        maxCount = Math.max(maxCount, counts[getIdx(c)]);
+    }
+    if (maxCount > Math.floor((length + 1) / 2)) {
+        return "";
+    }
+    const reorganizeArray = new Array(length);
+    let evenIndex = 0, oddIndex = 1;
+    const halfLength = Math.floor(length / 2);
+    for (let i = 0; i < 26; i++) {
+        const c = getAlpha('a'.charCodeAt() + i);
+        while (counts[i] > 0 && counts[i] <= halfLength && oddIndex < length) {
+            reorganizeArray[oddIndex] = c;
+            counts[i]--;
+            oddIndex += 2;
+        }
+        while (counts[i] > 0) {
+            reorganizeArray[evenIndex] = c;
+            counts[i]--;
+            evenIndex += 2;
+        }
+    }
+    return reorganizeArray.join('');
+};
+// @lc code=end
+
