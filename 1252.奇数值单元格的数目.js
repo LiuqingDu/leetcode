32fee788d752ae9427b5fbb340bd400d986e413f
Liuqing Du <me@duliuqing.com>
2025-02-25 19:53:25 -0600

    1252

---
+++ 1252.奇数值单元格的数目.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=1252 lang=javascript
+ *
+ * [1252] 奇数值单元格的数目
+ */
+
+// @lc code=start
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} indices
+ * @return {number}
+ */
+var oddCells = function(m, n, indices) {
+    const rows = new Array(m).fill(0);
+    const cols = new Array(n).fill(0);
+    for (const index of indices) {
+        rows[index[0]]++;
+        cols[index[1]]++;
+    }
+    let res = 0;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (((rows[i] + cols[j]) & 1) !== 0) {
+                res++;
+            }
+        }
+    }
+    return res;
+};
+// @lc code=end
+
