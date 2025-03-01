3cb3180ae2b0430978b0deb9d43ea30569786fa8
Liuqing Du <me@duliuqing.com>
2025-02-28 22:07:29 -0600

    1281

---
+++ 1281.整数的各位积和之差.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1281 lang=java
+ *
+ * [1281] 整数的各位积和之差
+ */
+
+// @lc code=start
+class Solution {
+    public int subtractProductAndSum(int n) {
+        // 按题目模拟
+
+        // m 是积, s 是和
+        int m = 1, s = 0;
+        while (n != 0) {
+            int x = n % 10;
+            n /= 10;
+            m *= x;
+            s += x;
+        }
+        return m - s;
+    }
+}
+// @lc code=end
+
