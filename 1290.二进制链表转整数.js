f45a85bac9c1520c9b853849c4b608b6823a798e
Liuqing Du <me@duliuqing.com>
2025-03-03 23:24:11 -0600

    1290

---
+++ 1290.二进制链表转整数.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=1290 lang=javascript
+ *
+ * [1290] 二进制链表转整数
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+    let res = 0;
+    while (head) {
+        res *= 2;
+        res += head.val;
+        head = head.next;
+    }
+    return res;
+};
+// @lc code=end
+
