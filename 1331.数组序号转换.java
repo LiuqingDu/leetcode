e3e937eb0ee92ef3f8d427a82724aaa0835f4d99
Liuqing Du <me@duliuqing.com>
2025-03-13 22:33:11 -0500

    1331

---
+++ 1331.数组序号转换.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=1331 lang=java
+ *
+ * [1331] 数组序号转换
+ */
+
+// @lc code=start
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        // 先排序,把数字和对应顺序放入 map
+        // 然后更新原数组
+        
+        int[] sortedArr = arr.clone();
+        Arrays.sort(sortedArr);
+        Map<Integer, Integer> ranks = new HashMap<Integer, Integer>();
+        int[] ans = new int[arr.length];
+        for (int a : sortedArr) {
+            if (!ranks.containsKey(a)) {
+                ranks.put(a, ranks.size() + 1);
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            ans[i] = ranks.get(arr[i]);
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
