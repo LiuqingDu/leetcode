fb8e7ed77bb2932e73e37f768959a70940a811c1
Liuqing Du <me@duliuqing.com>
2025-03-28 22:07:44 -0500

    1475

---
+++ 1475.商品折扣后的最终价格.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=1475 lang=javascript
+ *
+ * [1475] 商品折扣后的最终价格
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number[]}
+ */
+var finalPrices = function(prices) {
+    const n = prices.length;
+    const ans = new Array(n).fill(0);
+    const stack = [];
+    for (let i = n - 1; i >= 0; i--) {
+        while (stack.length && stack[stack.length - 1] > prices[i]) {
+            stack.pop();
+        }
+        ans[i] = stack.length === 0 ? prices[i] : prices[i] - stack[stack.length - 1];
+        stack.push(prices[i]);
+    }
+    return ans;
+};
+// @lc code=end
+
