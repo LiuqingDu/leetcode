21308db583fea466164c1a67989966508d87e9bf
Liuqing Du <me@duliuqing.com>
2025-04-12 21:08:49 -0500

    1408

---
+++ 1408.数组中的字符串匹配.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=1408 lang=java
+ *
+ * [1408] 数组中的字符串匹配
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> ans = new ArrayList<>();
+        int n = words.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (words[j].indexOf(words[i]) >= 0) {
+                    ans.add(words[i]);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
