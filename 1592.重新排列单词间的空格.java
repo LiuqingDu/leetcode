aa259e995ad73c23a0b9bc4dd8f3b25613c49415
Liuqing Du <me@duliuqing.com>
2025-05-04 22:01:52 -0500

    1592

---
+++ 1592.重新排列单词间的空格.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=1592 lang=java
+ *
+ * [1592] 重新排列单词间的空格
+ */
+
+// @lc code=start
+class Solution {
+    public String reorderSpaces(String text) {
+        // 统计单词和空格数量
+        // 如果单词数为 1，则将全部的空格拼接到这个单词后面即可。
+        // 否则先计算出单词间的间隔，并按照单词及间隔来进行拼接，若拼接后仍有多余的空格，则将剩下的空格拼接在末尾即可
+        int length = text.length();
+        String[] words = text.trim().split("\\s+");
+        int cntSpace = length;
+        for (String word : words) {
+            cntSpace -= word.length();
+        }
+        StringBuilder sb = new StringBuilder();
+        if (words.length == 1) {
+            sb.append(words[0]);
+            for (int i = 0; i < cntSpace; i++) {
+                sb.append(' ');
+            }
+            return sb.toString();
+        }
+        int perSpace = cntSpace / (words.length - 1);
+        int restSpace = cntSpace % (words.length - 1);
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0) {
+                for (int j = 0; j < perSpace; j++) {
+                    sb.append(' ');
+                }
+            }
+            sb.append(words[i]);
+        }
+        for (int i = 0; i < restSpace; i++) {
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
+}
+// @lc code=end
+
