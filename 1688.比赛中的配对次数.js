55f900b2859ce8594f7f8651d642df1054a1d1ae
Liuqing Du <me@duliuqing.com>
2025-05-05 22:40:49 -0500

    1688

---
+++ 1688.比赛中的配对次数.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=1688 lang=javascript
+ *
+ * [1688] 比赛中的配对次数
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numberOfMatches = function(n) {
+    return n - 1;
+};
+// @lc code=end
+
