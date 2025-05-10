a3a9819dc1dc52d53813c5322711e2d54890936c
Liuqing Du <me@duliuqing.com>
2025-05-09 22:38:03 -0500

    1710

---
+++ 1710.卡车上的最大单元数.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=1710 lang=javascript
+ *
+ * [1710] 卡车上的最大单元数
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} boxTypes
+ * @param {number} truckSize
+ * @return {number}
+ */
+var maximumUnits = function(boxTypes, truckSize) {
+    boxTypes.sort((a, b) => b[1] - a[1]);
+    let res = 0;
+    for (const boxType of boxTypes) {
+        let numberOfBoxes = boxType[0];
+        let numberOfUnitsPerBox = boxType[1];
+        if (numberOfBoxes < truckSize) {
+            res += numberOfBoxes * numberOfUnitsPerBox;
+            truckSize -= numberOfBoxes;
+        } else {
+            res += truckSize * numberOfUnitsPerBox;
+            break;
+        }
+    }
+    return res;
+};
+// @lc code=end
+
