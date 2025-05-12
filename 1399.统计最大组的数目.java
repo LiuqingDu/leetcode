d506b55ea5b19998183a76b62294844954469923
Liuqing Du <me@duliuqing.com>
2025-05-12 15:29:13 -0500

    1399

---
+++ 1399.统计最大组的数目.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=1399 lang=java
+ *
+ * [1399] 统计最大组的数目
+ */
+
+// @lc code=start
+class Solution {
+    public int countLargestGroup(int n) {
+        Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        int maxValue = 0;
+        for (int i = 1; i <= n; ++i) {
+            int key = 0, i0 = i;
+            while (i0 != 0) {
+                key += i0 % 10;
+                i0 /= 10;
+            }
+            hashMap.put(key, hashMap.getOrDefault(key, 0) + 1);
+            maxValue = Math.max(maxValue, hashMap.get(key));
+        }
+        int count = 0;
+        for (Map.Entry<Integer, Integer> kvpair : hashMap.entrySet()) {
+            if (kvpair.getValue() == maxValue) {
+                ++count;
+            }
+        }
+        return count;
+    }
+}
+// @lc code=end
+
