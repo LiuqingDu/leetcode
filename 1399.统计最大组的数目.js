8ec7cd0b4c7aa4db90bbf1700dbf90b8d4dfd034
Liuqing Du <me@duliuqing.com>
2025-05-12 15:29:22 -0500

    1399

---
+++ 1399.统计最大组的数目.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=1399 lang=javascript
+ *
+ * [1399] 统计最大组的数目
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countLargestGroup = function(n) {
+    let hashMap = {};
+    let maxValue = 0;
+    for (let i = 1; i <= n; ++i) {
+        let key = 0, i0 = i;
+        while (i0) {
+            key += i0 % 10;
+            i0 = Math.floor(i0 / 10);
+        }
+        hashMap[key] = (hashMap[key] || 0) + 1;
+        maxValue = Math.max(maxValue, hashMap[key]);
+    }
+
+    let count = 0;
+    for (let value of Object.values(hashMap)) {
+        if (value === maxValue) {
+            count++;
+        }
+    }
+    return count;
+};
+// @lc code=end
+
