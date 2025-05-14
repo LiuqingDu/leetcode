87f998e63b6ffd905a667f616c8edb5f2c896c95
Liuqing Du <me@duliuqing.com>
2025-05-14 16:47:49 -0500

    1742

---
+++ 1742.盒子中小球的最大数量.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=1742 lang=java
+ *
+ * [1742] 盒子中小球的最大数量
+ */
+
+// @lc code=start
+class Solution {
+    public int countBalls(int lowLimit, int highLimit) {
+        Map<Integer, Integer> count = new HashMap<Integer, Integer>();
+        int res = 0;
+        for (int i = lowLimit; i <= highLimit; i++) {
+            int box = 0, x = i;
+            while (x != 0) {
+                box += x % 10;
+                x /= 10;
+            }
+            count.put(box, count.getOrDefault(box, 0) + 1);
+            res = Math.max(res, count.get(box));
+        }
+        return res;
+    }
+}
+// @lc code=end
+
