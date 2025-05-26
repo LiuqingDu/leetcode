f10b696e4af7fef432e6cf25d2d5acfe66b8b8cb
Liuqing Du <me@duliuqing.com>
2025-05-26 15:26:17 -0500

    1818

---
+++ 1816.截断句子.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=1816 lang=java
+ *
+ * [1816] 截断句子
+ */
+
+// @lc code=start
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int n = s.length();
+        int end = 0, count = 0;
+        // 分割单词
+        for (int i = 1; i <= n; i++) {
+            if (i == n || s.charAt(i) == ' ') {
+                count++;
+                if (count == k) {
+                    end = i;
+                    break;
+                }
+            }
+        }
+        return s.substring(0, end);
+    }
+}
+// @lc code=end
+
