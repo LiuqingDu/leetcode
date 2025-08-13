4c68d1101e9f10a27a7eb199c55bbe65bbcbb0b8
Liuqing Du <me@duliuqing.com>
2025-08-13 13:04:45 -0500

    863

---
+++ 863.二叉树中所有距离为-k-的结点.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=863 lang=java
+ *
+ * [863] 二叉树中所有距离为 K 的结点
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // 若将 target 当作树的根结点，我们就能从 target 出发，使用深度优先搜索去寻找与 target 距离为 k 的所有结点，即深度为 k 的所有结点。
+    // 由于输入的二叉树没有记录父结点，为此，我们从根结点 root 出发，使用深度优先搜索遍历整棵树，同时用一个哈希表记录每个结点的父结点。
+    // 然后从 target 出发，使用深度优先搜索遍历整棵树，除了搜索左右儿子外，还可以顺着父结点向上搜索。
+    // 代码实现时，由于每个结点值都是唯一的，哈希表的键可以用结点值代替。此外，为避免在深度优先搜索时重复访问结点，递归时额外传入来源结点 from，在递归前比较目标结点是否与来源结点相同，不同的情况下才进行递归。
+
+    Map<Integer, TreeNode> parents = new HashMap<Integer, TreeNode>();
+    List<Integer> ans = new ArrayList<Integer>();
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        // 从 root 出发 DFS，记录每个结点的父结点
+        findParents(root);
+
+        // 从 target 出发 DFS，寻找所有深度为 k 的结点
+        findAns(target, null, 0, k);
+
+        return ans;
+    }
+
+    public void findParents(TreeNode node) {
+        if (node.left != null) {
+            parents.put(node.left.val, node);
+            findParents(node.left);
+        }
+        if (node.right != null) {
+            parents.put(node.right.val, node);
+            findParents(node.right);
+        }
+    }
+
+    public void findAns(TreeNode node, TreeNode from, int depth, int k) {
+        if (node == null) {
+            return;
+        }
+        if (depth == k) {
+            ans.add(node.val);
+            return;
+        }
+        if (node.left != from) {
+            findAns(node.left, node, depth + 1, k);
+        }
+        if (node.right != from) {
+            findAns(node.right, node, depth + 1, k);
+        }
+        if (parents.get(node.val) != from) {
+            findAns(parents.get(node.val), node, depth + 1, k);
+        }
+    }
+}
+// @lc code=end
+
