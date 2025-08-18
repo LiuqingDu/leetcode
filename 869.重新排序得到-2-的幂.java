c92f964fa05c175948289b04a6b302931fea7900
Liuqing Du <me@duliuqing.com>
2025-08-18 18:00:24 -0500

    869

---
+++ 869.重新排序得到-2-的幂.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=869 lang=java
+ *
+ * [869] 重新排序得到 2 的幂
+ */
+
+// @lc code=start
+class Solution {
+    boolean[] vis;
+
+    public boolean reorderedPowerOf2(int n) {
+        char[] nums = Integer.toString(n).toCharArray();
+        Arrays.sort(nums);
+        vis = new boolean[nums.length];
+        return backtrack(nums, 0, 0);
+    }
+
+    public boolean backtrack(char[] nums, int idx, int num) {
+        if (idx == nums.length) {
+            return isPowerOfTwo(num);
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            // 不能有前导零
+            if ((num == 0 && nums[i] == '0') || vis[i] || (i > 0 && !vis[i - 1] && nums[i] == nums[i - 1])) {
+                continue;
+            }
+            vis[i] = true;
+            if (backtrack(nums, idx + 1, num * 10 + nums[i] - '0')) {
+                return true;
+            }
+            vis[i] = false;
+        }
+        return false;
+    }
+
+    public boolean isPowerOfTwo(int n) {
+        return (n & (n - 1)) == 0;
+    }
+}
+// @lc code=end
+
