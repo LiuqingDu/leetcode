19d7013893ec2fac958c6b7101061597de3f9792
Liuqing Du <me@duliuqing.com>
2025-10-25 20:50:41 -0500

    2379

---
+++ 2379.得到-k-个黑块的最少涂色次数.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=2379 lang=javascript
+ *
+ * [2379] 得到 K 个黑块的最少涂色次数
+ */
+
+// @lc code=start
+/**
+ * @param {string} blocks
+ * @param {number} k
+ * @return {number}
+ */
+var minimumRecolors = function(blocks, k) {
+    let l = 0, r = 0, cnt = 0;
+    while (r < k) {
+        cnt += blocks[r] === 'W' ? 1 : 0;
+        r++;
+    }
+    let res = cnt;
+    while (r < blocks.length) {
+        cnt += blocks[r] === 'W' ? 1 : 0;
+        cnt -= blocks[l] === 'W' ? 1 : 0;
+        res = Math.min(res, cnt);
+        l++;
+        r++;
+    }
+    return res;
+};
+// @lc code=end
+
