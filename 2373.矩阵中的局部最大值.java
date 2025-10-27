3a2651b9c60f72f1161f1041adae863712e4114a
Liuqing Du <me@duliuqing.com>
2025-10-26 22:34:07 -0500

    2373

---
+++ 2373.矩阵中的局部最大值.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=2373 lang=java
+ *
+ * [2373] 矩阵中的局部最大值
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] largestLocal(int[][] grid) {
+        int n = grid.length;
+        int[][] res = new int[n - 2][n - 2];
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                for (int x = i; x < i + 3; x++) {
+                    for (int y = j; y < j + 3; y++) {
+                        res[i][j] = Math.max(res[i][j], grid[x][y]);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
