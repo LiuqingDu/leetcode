a340d5021187eadaed49a7bf8b95ebc1495597af
Liuqing Du <me@duliuqing.com>
2025-10-29 22:21:37 -0500

    2395

---
+++ 2395.和相等的子数组.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=2395 lang=javascript
+ *
+ * [2395] 和相等的子数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var findSubarrays = function(nums) {
+    const n = nums.length;
+    const seen = new Set();
+    for (let i = 0; i < n - 1; ++i) {
+        let sum = nums[i] + nums[i + 1];
+        if (seen.has(sum)) {
+            return true;
+        }
+        seen.add(sum);
+    }
+    return false;
+};
+// @lc code=end
+
