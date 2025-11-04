a3c2e05b8f3a2f8ea9ae5b92ce40a4640056b0f4
Liuqing Du <me@duliuqing.com>
2025-11-04 17:57:56 -0600

    2423

---
+++ 2423.删除字符使频率相同.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=2423 lang=java
+ *
+ * [2423] 删除字符使频率相同
+ */
+
+// @lc code=start
+class Solution {
+    public boolean equalFrequency(String word) {
+        
+        int[] charCount = new int[26];
+        int n = word.length();
+        for (int i = 0; i < n; i++) {
+            charCount[word.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (charCount[i] == 0) {
+                continue;
+            }
+            charCount[i]--;
+            HashSet<Integer> frequency = new HashSet<Integer>();
+            for (int f : charCount) {
+                if (f > 0) {
+                    frequency.add(f);
+                }
+            }
+            if (frequency.size() == 1) {
+                return true;
+            }
+            charCount[i]++;
+        }
+        return false;
+    }
+}
+// @lc code=end
+
