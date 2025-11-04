00a14ee0e7fdcf81f383f4292af6ce7b3947a7bc
Liuqing Du <me@duliuqing.com>
2025-11-03 21:55:59 -0600

    2418

---
+++ 2418.按身高排序.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=2418 lang=javascript
+ *
+ * [2418] 按身高排序
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} names
+ * @param {number[]} heights
+ * @return {string[]}
+ */
+var sortPeople = function(names, heights) {
+    const n = names.length;
+    const indices = Array.from({length: n}, (_, i) => i);
+    indices.sort((a, b) => heights[b] - heights[a]);
+    const res = new Array(n);
+    for (let i = 0; i < n; i++) {
+        res[i] = names[indices[i]];
+    }
+    return res;
+};
+// @lc code=end
+
