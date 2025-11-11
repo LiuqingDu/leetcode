369c6a69b07780d4fb5bcd426167f9dbe58d6a73
Liuqing Du <me@duliuqing.com>
2025-11-10 20:56:50 -0600

    2451

---
+++ 2451.差值数组不同的字符串.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=2451 lang=java
+ *
+ * [2451] 差值数组不同的字符串
+ */
+
+// @lc code=start
+class Solution {
+    public String oddString(String[] words) {
+        int[] diff0 = get(words[0]);
+        int[] diff1 = get(words[1]);
+        if (Arrays.equals(diff0, diff1)) {
+            for (int i = 2; i < words.length; i++) {
+                if (!Arrays.equals(diff0, get(words[i]))) {
+                    return words[i];
+                }
+            }
+        }
+        return Arrays.equals(diff0, get(words[2])) ? words[1] : words[0];
+    }
+
+    public int[] get(String word) {
+        int[] diff = new int[word.length() - 1];
+        for (int i = 0; i + 1 < word.length(); i++) {
+            diff[i] = word.charAt(i + 1) - word.charAt(i);
+        }
+        return diff;
+    }
+}
+// @lc code=end
+
