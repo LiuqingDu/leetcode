84392fbcc50e60758791fe288b1978e4d01cf259
Liuqing Du <me@duliuqing.com>
2025-11-15 22:12:06 -0600

    2475

---
+++ 2475.数组中不等三元组的数目.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=2475 lang=java
+ *
+ * [2475] 数组中不等三元组的数目
+ */
+
+// @lc code=start
+class Solution {
+    public int unequalTriplets(int[] nums) {
+        int res = 0, n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k]) {
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
