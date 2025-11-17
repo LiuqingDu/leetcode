69cd6f20ff0e19abffc9a4106577b639ed1383f2
Liuqing Du <me@duliuqing.com>
2025-11-16 22:27:43 -0600

    2481

---
+++ 2481.分割圆的最少切割次数.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=2481 lang=java
+ *
+ * [2481] 分割圆的最少切割次数
+ */
+
+// @lc code=start
+class Solution {
+    public int numberOfCuts(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return n / 2;
+        }
+        return n;
+    }
+}
+// @lc code=end
+
