348ff74443d9d9b3d350afa3d8f71e3c3e60ce78
Liuqing Du <me@duliuqing.com>
2025-11-20 15:37:16 -0600

    2500

---
+++ 2500.删除每行中的最大值.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=2500 lang=java
+ *
+ * [2500] 删除每行中的最大值
+ */
+
+// @lc code=start
+class Solution {
+    public int deleteGreatestValue(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            Arrays.sort(grid[i]);
+        }
+        int res = 0;
+        for (int j = 0; j < n; j++) {
+            int mx = 0;
+            for (int i = 0; i < m; i++) {
+                mx = Math.max(mx, grid[i][j]);
+            }
+            res += mx;
+        }
+        return res;
+    }
+}
+// @lc code=end
+
