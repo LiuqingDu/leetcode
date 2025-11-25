60f5fe2cd8a4c645b5a9defe5e97349a702916ad
Liuqing Du <me@duliuqing.com>
2025-11-25 13:02:54 -0600

    979

---
+++ 979.在二叉树中分配硬币.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=979 lang=java
+ *
+ * [979] 在二叉树中分配硬币
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int move = 0;
+
+    public int distributeCoins(TreeNode root) {
+        dfs(root);
+        return move;
+    }
+
+    public int dfs(TreeNode root) {
+        int moveleft = 0;
+        int moveright = 0;
+        if (root == null) {
+            return 0;
+        }
+        if (root.left != null) {
+            moveleft = dfs(root.left);
+        }        
+        if (root.right != null) {
+            moveright = dfs(root.right);
+        }
+        move += Math.abs(moveleft) + Math.abs(moveright);
+        return moveleft + moveright + root.val - 1;
+    }
+}
+// @lc code=end
+
