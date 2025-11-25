08ec9fada34d87575a7c6d2df88e206120ed92cc
Liuqing Du <me@duliuqing.com>
2025-11-24 21:51:18 -0600

    2525

---
+++ 2525.根据规则将箱子分类.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=2525 lang=javascript
+ *
+ * [2525] 根据规则将箱子分类
+ */
+
+// @lc code=start
+/**
+ * @param {number} length
+ * @param {number} width
+ * @param {number} height
+ * @param {number} mass
+ * @return {string}
+ */
+var categorizeBox = function(length, width, height, mass) {
+    const maxd = Math.max(length, width, height);
+    const vol = length * width * height;
+    const isBulky = maxd >= 10000 || vol >= 10**9;
+    const isHeavy = mass >= 100;
+    if (isBulky && isHeavy) {
+       return 'Both';
+    } else if (isBulky) {
+        return 'Bulky';
+    } else if (isHeavy) {
+        return 'Heavy';
+    } else {
+        return 'Neither';
+    }
+};
+// @lc code=end
+
