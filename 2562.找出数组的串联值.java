eac0b5b10b26f6206e699082d2d7c6980edc354b
Liuqing Du <me@duliuqing.com>
2025-12-02 23:11:45 -0600

    2562

---
+++ 2562.找出数组的串联值.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=2562 lang=java
+ *
+ * [2562] 找出数组的串联值
+ */
+
+// @lc code=start
+class Solution {
+    public long findTheArrayConcVal(int[] nums) {
+        long ans = 0;
+        for (int i = 0, j = nums.length - 1; i <= j; i++, j--) {
+            if (i != j) {
+                ans += Integer.parseInt(Integer.toString(nums[i]) + Integer.toString(nums[j]));
+            } else {
+                ans += nums[i];
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
