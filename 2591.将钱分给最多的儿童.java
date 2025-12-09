79681b04b2dc958601cfe933082fb810c990a4a8
Liuqing Du <me@duliuqing.com>
2025-12-08 21:18:20 -0600

    2591

---
+++ 2591.将钱分给最多的儿童.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=2591 lang=java
+ *
+ * [2591] 将钱分给最多的儿童
+ */
+
+// @lc code=start
+class Solution {
+    public int distMoney(int money, int children) {
+        if (money < children) {
+            return -1;
+        }
+        money -= children;
+        int cnt = Math.min(money / 7, children);
+        money -= cnt * 7;
+        children -= cnt;
+        if ((children == 0 && money > 0) || (children == 1 && money == 3)) {
+            cnt--;
+        }
+        return cnt;
+    }
+}
+// @lc code=end
+
