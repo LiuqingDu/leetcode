1187d3e20d8a1d87cc202dcc61518b0d66b0bc86
Liuqing Du <me@duliuqing.com>
2025-12-11 21:29:36 -0600

    2609

---
+++ 2609.最长平衡子字符串.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=2609 lang=java
+ *
+ * [2609] 最长平衡子字符串
+ */
+
+// @lc code=start
+class Solution {
+    public int findTheLongestBalancedSubstring(String s) {
+        int res = 0;
+        int n = s.length();
+        int[] count = new int[2];
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1') {
+                count[1]++;
+                res = Math.max(res, 2 * Math.min(count[0], count[1]));
+            } else if (i == 0 || s.charAt(i - 1) == '1') {
+                count[0] = 1;
+                count[1] = 0;
+            } else {
+                count[0]++;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
