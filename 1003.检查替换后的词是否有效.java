1ee1dd52cc4e730a2e6b358813d897dcd0f586aa
Liuqing Du <me@duliuqing.com>
2025-12-14 21:31:40 -0600

    1003

---
+++ 1003.检查替换后的词是否有效.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=1003 lang=java
+ *
+ * [1003] 检查替换后的词是否有效
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isValid(String s) {
+        StringBuilder stk = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            stk.append(c);
+            if (stk.length() >= 3 && stk.substring(stk.length() - 3).equals("abc")) {
+                stk.delete(stk.length() - 3, stk.length());
+            }
+        }
+        return stk.isEmpty();
+    }
+}
+// @lc code=end
+
