b0daa095fe434ebe9e587f2219a183795533230c
Liuqing Du <me@duliuqing.com>
2025-12-30 14:34:33 -0600

    2706

---
+++ 2706.购买两块巧克力.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=2706 lang=java
+ *
+ * [2706] 购买两块巧克力
+ */
+
+// @lc code=start
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int fi = Integer.MAX_VALUE, se = Integer.MAX_VALUE;
+        for (int price : prices) {
+            if (price < fi) {
+                se = fi;
+                fi = price;
+            } else if (price < se) {
+                se = price;
+            }
+        }
+        return money < fi + se ? money : money - fi - se;
+    }
+}
+// @lc code=end
+
