fba07e4d3e36e212c54fe18c610ed72455f60943
Liuqing Du <me@duliuqing.com>
2026-01-06 21:06:54 -0600

    2744

---
+++ 2744.最大字符串配对数目.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=2744 lang=java
+ *
+ * [2744] 最大字符串配对数目
+ */
+
+// @lc code=start
+class Solution {
+    public int maximumNumberOfStringPairs(String[] words) {
+        int n = words.length;
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (words[i].charAt(0) == words[j].charAt(1) && words[i].charAt(1) == words[j].charAt(0)) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
