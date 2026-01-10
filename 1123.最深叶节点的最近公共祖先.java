5416b0c09e583159270e5bdfdada0a2a55139ed3
Liuqing Du <me@duliuqing.com>
2026-01-09 20:44:21 -0600

    1123

---
+++ 1123.最深叶节点的最近公共祖先.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=1123 lang=java
+ *
+ * [1123] 最深叶节点的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // 本质就是比较左右子节点的深度, 如果深度一致说明当前节点就是最近公共祖先,
+    // 否则选择更深的子节点继续遍历, 接着比较它的左右子节点深度.
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        return f(root).getKey();
+    }
+
+    private Pair<TreeNode, Integer> f(TreeNode root) {
+        if (root == null) {
+            return new Pair<>(root, 0);
+        }
+
+        Pair<TreeNode, Integer> left = f(root.left);
+        Pair<TreeNode, Integer> right = f(root.right);
+
+        if (left.getValue() > right.getValue()) {
+            return new Pair<>(left.getKey(), left.getValue() + 1);
+        }
+        if (left.getValue() < right.getValue()) {
+            return new Pair<>(right.getKey(), right.getValue() + 1);
+        }
+        return new Pair<>(root, left.getValue() + 1);
+    }
+}
+// @lc code=end
+
