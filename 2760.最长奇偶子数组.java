98853624f505b8c813d599c170d1f153060be20a
Liuqing Du <me@duliuqing.com>
2026-01-10 22:04:06 -0600

    2760

---
+++ 2760.最长奇偶子数组.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=2760 lang=java
+ *
+ * [2760] 最长奇偶子数组
+ */
+
+// @lc code=start
+class Solution {
+    public int longestAlternatingSubarray(int[] nums, int threshold) {
+        int res = 0, n = nums.length;
+        for (int l = 0; l < n; l++) {
+            for (int r = l; r < n; r++) {
+                if (isSatisfied(nums, l, r, threshold)) {
+                    res = Math.max(res, r - l + 1);
+                }
+            }
+        }
+        return res;
+    }
+
+    public boolean isSatisfied(int[] nums, int l, int r, int threshold) {
+        if (nums[l] % 2 != 0) {
+            return false;
+        }
+        for (int i = l; i <= r; i++) {
+            if (nums[i] > threshold || (i < r && nums[i] % 2 == nums[i + 1] % 2)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
+
