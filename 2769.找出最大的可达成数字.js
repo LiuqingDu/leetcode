2580b3e4bb864f3fb8d9bd67052294d2cc57f916
Liuqing Du <me@duliuqing.com>
2026-01-11 22:54:07 -0600

    2769

---
+++ 2769.找出最大的可达成数字.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=2769 lang=javascript
+ *
+ * [2769] 找出最大的可达成数字
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function(num, t) {
+    return num + 2 * t;
+};
+// @lc code=end
+
