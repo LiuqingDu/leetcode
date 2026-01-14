f83db354351760d69f21305e069fa1acaf442388
Liuqing Du <me@duliuqing.com>
2026-01-13 21:54:46 -0600

    1124

---
+++ 1124.表现良好的最长时间段.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=1124 lang=java
+ *
+ * [1124] 表现良好的最长时间段
+ */
+
+// @lc code=start
+class Solution {
+    public int longestWPI(int[] hours) {
+        int n = hours.length;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int s = 0, res = 0;
+        for (int i = 0; i < n; i++) {
+            s += hours[i] > 8 ? 1 : -1;
+            if (s > 0) {
+                res = Math.max(res, i + 1);
+            } else {
+                if (map.containsKey(s - 1)) {
+                    res = Math.max(res, i - map.get(s - 1));
+                }
+            }
+            if (!map.containsKey(s)) {
+                map.put(s, i);
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
