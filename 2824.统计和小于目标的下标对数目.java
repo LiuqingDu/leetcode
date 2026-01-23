3ee9fe4bebb7114a8cf6f92e4d5d8565950412f2
Liuqing Du <me@duliuqing.com>
2026-01-22 21:18:53 -0600

    2824

---
+++ 2824.统计和小于目标的下标对数目.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=2824 lang=java
+ *
+ * [2824] 统计和小于目标的下标对数目
+ */
+
+// @lc code=start
+class Solution {
+    public int countPairs(List<Integer> nums, int target) {
+        int res = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums.get(i) + nums.get(j) < target) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
