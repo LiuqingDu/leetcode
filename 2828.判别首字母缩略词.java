653c63f264e625e3812f2a8f7e6eedef08e902a4
Liuqing Du <me@duliuqing.com>
2026-01-24 21:48:33 -0600

    2828

---
+++ 2828.判别首字母缩略词.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=2828 lang=java
+ *
+ * [2828] 判别首字母缩略词
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isAcronym(List<String> words, String s) {
+        if (words.size() != s.length()) {
+            return false;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (words.get(i).charAt(0) != s.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
+
