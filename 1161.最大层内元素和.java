3a07682ef4cf430cf760f3c50b5186d1fcebb476
Liuqing Du <me@duliuqing.com>
2026-02-21 22:18:49 -0600

    1161

---
+++ 1161.最大层内元素和.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=1161 lang=java
+ *
+ * [1161] 最大层内元素和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // DFS
+    
+    private List<Integer> sum = new ArrayList<Integer>();
+
+    public int maxLevelSum(TreeNode root) {
+        dfs(root, 0);
+        int ans = 0;
+        for (int i = 0; i < sum.size(); ++i) {
+            if (sum.get(i) > sum.get(ans)) {
+                ans = i;
+            }
+        }
+        return ans + 1; // 层号从 1 开始
+    }
+
+    private void dfs(TreeNode node, int level) {
+        if (level == sum.size()) {
+            sum.add(node.val);
+        } else {
+            sum.set(level, sum.get(level) + node.val);
+        }
+        if (node.left != null) {
+            dfs(node.left, level + 1);
+        }
+        if (node.right != null) {
+            dfs(node.right, level + 1);
+        }
+    }
+}
+// @lc code=end
+
