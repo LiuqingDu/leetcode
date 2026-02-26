525341daa6ffbbd87b95911155ae089475c6c098
Liuqing Du <me@duliuqing.com>
2026-02-25 20:18:32 -0600

    1487

---
+++ 1487.保证文件名唯一.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=1487 lang=java
+ *
+ * [1487] 保证文件名唯一
+ */
+
+// @lc code=start
+class Solution {
+    public String[] getFolderNames(String[] names) {
+        Map<String, Integer> index = new HashMap<String, Integer>();
+        int n = names.length;
+        String[] res = new String[n];
+        for (int i = 0; i < n; i++) {
+            String name = names[i];
+            if (!index.containsKey(name)) {
+                res[i] = name;
+                index.put(name, 1);
+            } else {
+                int k = index.get(name);
+                while (index.containsKey(addSuffix(name, k))) {
+                    k++;
+                }
+                res[i] = addSuffix(name, k);
+                index.put(name, k + 1);
+                index.put(addSuffix(name, k), 1);
+            }
+        }
+        return res;
+    }
+
+    public String addSuffix(String name, int k) {
+        return name + "(" + k + ")";
+    }
+}
+// @lc code=end
+
