a872546ce944959c613f60ba81e38f0cd6dab37f
Liuqing Du <me@duliuqing.com>
2026-03-10 22:21:29 -0500

    1267

---
+++ 1267.统计参与通信的服务器.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=1267 lang=java
+ *
+ * [1267] 统计参与通信的服务器
+ */
+
+// @lc code=start
+class Solution {
+    public int countServers(int[][] grid) {
+        
+        int m = grid.length, n = grid[0].length;
+        Map<Integer, Integer> rows = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> cols = new HashMap<Integer, Integer>();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) {
+                    rows.put(i, rows.getOrDefault(i, 0) + 1);
+                    cols.put(j, cols.getOrDefault(j, 0) + 1);
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1 && (rows.get(i) > 1 || cols.get(j) > 1)) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
