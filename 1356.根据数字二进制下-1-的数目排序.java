501014373d1e6d0a0b28c81aff4eb58c37e6050a
Liuqing Du <me@duliuqing.com>
2026-07-08 20:11:22 -0500

    1356

---
+++ 1356.根据数字二进制下-1-的数目排序.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=1356 lang=java
+ *
+ * [1356] 根据数字二进制下 1 的数目排序
+ */
+
+// @lc code=start
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        
+        int[] bit = new int[10001];
+        List<Integer> list = new ArrayList<Integer>();
+        for (int x : arr) {
+            list.add(x);
+            bit[x] = get(x);
+        }
+        Collections.sort(list, new Comparator<Integer>() {
+            public int compare(Integer x, Integer y) {
+                if (bit[x] != bit[y]) {
+                    return bit[x] - bit[y];
+                } else {
+                    return x - y;
+                }
+            }
+        });
+        for (int i = 0; i < arr.length; ++i) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+
+    public int get(int x) {
+        int res = 0;
+        while (x != 0) {
+            res += x % 2;
+            x /= 2;
+        }
+        return res;
+    }
+}
+// @lc code=end
+
