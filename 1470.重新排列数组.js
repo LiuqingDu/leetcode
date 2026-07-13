e04431b24129ff23837dfeb44ac1979a52253963
Liuqing Du <me@duliuqing.com>
2026-07-12 21:39:24 -0500

    147

---
+++ 1470.重新排列数组.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=1470 lang=javascript
+ *
+ * [1470] 重新排列数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    let res = new Array(n * 2).fill(0);
+    for (let i = 0; i < n; i++) {
+        res[i * 2] = nums[i]
+        res[i + 2 + 1] = nums[i + n];
+    }
+    return res;
+};
+// @lc code=end
+
