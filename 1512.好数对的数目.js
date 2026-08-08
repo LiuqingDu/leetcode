7bba4c536d4418cfba758dfd2324ef031444795a
Liuqing Du <me@duliuqing.com>
2026-08-07 22:38:57 -0500

    1512

---
+++ 1512.好数对的数目.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1512 lang=javascript
+ *
+ * [1512] 好数对的数目
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    let res = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                res++;
+            }
+        }
+    }
+    return res;
+};
+// @lc code=end
+
