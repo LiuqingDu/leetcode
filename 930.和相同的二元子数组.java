69e73d4cf5e22c4f291176939db850a2891fa339
Liuqing Du <me@duliuqing.com>
2026-08-09 21:37:38 -0500

    930

---
+++ 930.和相同的二元子数组.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=930 lang=java
+ *
+ * [930] 和相同的二元子数组
+ */
+
+// @lc code=start
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        
+        int sum = 0;
+        Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+        int ret = 0;
+        for (int num : nums) {
+            cnt.put(sum, cnt.getOrDefault(sum, 0) + 1);
+            sum += num;
+            ret += cnt.getOrDefault(sum - goal, 0);
+        }
+        return ret;
+
+    }
+}
+// @lc code=end
+
