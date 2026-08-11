35f0d1aada634819a2ff9eea30656a0b54615b4c
Liuqing Du <me@duliuqing.com>
2026-08-10 21:28:52 -0500

    970

---
+++ 970.强整数.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=970 lang=java
+ *
+ * [970] 强整数
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        
+        Set<Integer> set = new HashSet<Integer>();
+        int value1 = 1;
+        for (int i = 0; i < 21; i++) {
+            int value2 = 1;
+            for (int j = 0; j < 21; j++) {
+                int value = value1 + value2;
+                if (value <= bound) {
+                    set.add(value);
+                } else {
+                    break;
+                }
+                value2 *= y;
+            }
+            if (value1 > bound) {
+                break;
+            }
+            value1 *= x;
+        }
+        return new ArrayList<Integer>(set);
+
+    }
+}
+// @lc code=end
+
