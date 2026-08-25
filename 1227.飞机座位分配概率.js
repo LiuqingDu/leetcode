7bc67c258f3423c9ac3dab56f2da7172eef57ed1
Liuqing Du <me@duliuqing.com>
2026-08-24 21:21:39 -0500

    1227

---
+++ 1227.飞机座位分配概率.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=1227 lang=javascript
+ *
+ * [1227] 飞机座位分配概率
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthPersonGetsNthSeat = function(n) {
+    return n === 1 ? 1.0 : 0.5;
+};
+// @lc code=end
+
