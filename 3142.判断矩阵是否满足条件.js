58b2f868387b9fb7b949de32294a2d4fb28779e7
Liuqing Du <me@duliuqing.com>
2026-08-30 22:02:40 -0500

    3142

---
+++ 3142.判断矩阵是否满足条件.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=3142 lang=javascript
+ *
+ * [3142] 判断矩阵是否满足条件
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} grid
+ * @return {boolean}
+ */
+var satisfiesConditions = function(grid) {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (i + 1 < grid.length && grid[i][j] !== grid[i + 1][j]) {
+                return false;
+            }
+            if (j + 1 < grid[0].length && grid[i][j] === grid[i][j + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+// @lc code=end
+
