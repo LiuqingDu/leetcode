6bef7fe87c59b2d3df2fcd3052532a9192d22fc2
Liuqing Du <me@duliuqing.com>
2026-09-08 21:11:52 -0500

    1024

---
+++ 1024.视频拼接.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=1024 lang=java
+ *
+ * [1024] 视频拼接
+ */
+
+// @lc code=start
+class Solution {
+    public int videoStitching(int[][] clips, int time) {
+        
+        int[] dp = new int[time + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE - 1);
+        dp[0] = 0;
+        for (int i = 1; i <= time; i++) {
+            for (int[] clip : clips) {
+                if (clip[0] < i && i <= clip[1]) {
+                    dp[i] = Math.min(dp[i], dp[clip[0]] + 1);
+                }
+            }
+        }
+        return dp[time] == Integer.MAX_VALUE - 1 ? -1 : dp[time];
+    }
+}
+// @lc code=end
+
