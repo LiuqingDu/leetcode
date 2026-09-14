4cc9b081b06003a7a20b640f623f63992f929f4b
Liuqing Du <me@duliuqing.com>
2026-09-13 21:07:39 -0500

    1253

---
+++ 1253.重构-2-行二进制矩阵.js
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=1253 lang=javascript
+ *
+ * [1253] 重构 2 行二进制矩阵
+ */
+
+// @lc code=start
+/**
+ * @param {number} upper
+ * @param {number} lower
+ * @param {number[]} colsum
+ * @return {number[][]}
+ */
+var reconstructMatrix = function(upper, lower, colsum) {
+    
+    let n = colsum.length;
+    let sumVal = 0;
+    let twoNum = 0;
+    for (let i = 0; i < n; i++) {
+        if (colsum[i] == 2) {
+            twoNum++;
+        }
+        sumVal += colsum[i];
+    }
+    if (sumVal != upper + lower || Math.min(upper, lower) < twoNum) {
+        return [];
+    }
+    upper -= twoNum;
+    lower -= twoNum;
+    let res = Array.from({ length: 2 }, () => new Array(n).fill(0));
+    for (let i = 0; i < n; i++) {
+        if (colsum[i] == 2) {
+            res[0][i] = res[1][i] = 1;
+        } else if (colsum[i] == 1) {
+            if (upper > 0) {
+                res[0][i] = 1;
+                upper--;
+            } else {
+                res[1][i] = 1;
+            }
+        }
+    }
+    return res;
+
+};
+// @lc code=end
+
