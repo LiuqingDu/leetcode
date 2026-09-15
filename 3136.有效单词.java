c227545169d70452938fa027c85f2db35aa021e9
Liuqing Du <me@duliuqing.com>
2026-09-14 21:39:06 -0500

    3136

---
+++ 3136.有效单词.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=3136 lang=java
+ *
+ * [3136] 有效单词
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isValid(String word) {
+        
+        if (word.length() < 3) {
+            return false;
+        }
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+        for (char c : word.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char ch = Character.toLowerCase(c);
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            } else if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return hasVowel && hasConsonant;
+
+    }
+}
+// @lc code=end
+
