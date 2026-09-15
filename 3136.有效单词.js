fee8159748086e4a2764f4a8187812d4be4e26e9
Liuqing Du <me@duliuqing.com>
2026-09-14 21:39:11 -0500

    3136

---
+++ 3136.有效单词.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=3136 lang=javascript
+ *
+ * [3136] 有效单词
+ */
+
+// @lc code=start
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var isValid = function(word) {
+    
+    if (word.length < 3) {
+        return false;
+    }
+    let hasVowel = false;
+    let hasConsonant = false;
+    for (const c of word) {
+        if (/[a-zA-Z]/.test(c)) {
+            const ch = c.toLowerCase();
+            if (ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u') {
+                hasVowel = true;
+            } else {
+                hasConsonant = true;
+            }
+        } else if (!/\d/.test(c)) {
+            return false;
+        }
+    }
+    return hasVowel && hasConsonant;
+
+};
+// @lc code=end
+
