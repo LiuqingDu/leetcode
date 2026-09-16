8ce2ebf7495bb979344baf84d92c0b70cddfbc95
Liuqing Du <me@duliuqing.com>
2026-09-15 21:04:37 -0500

    1218

---
+++ 1218.最长定差子序列.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=1218 lang=java
+ *
+ * [1218] 最长定差子序列
+ */
+
+// @lc code=start
+class Solution {
+    public int longestSubsequence(int[] arr, int difference) {
+        
+        int ans = 0;
+        Map<Integer, Integer> dp = new HashMap<Integer, Integer>();
+        for (int v : arr) {
+            dp.put(v, dp.getOrDefault(v - difference, 0) + 1);
+            ans = Math.max(ans, dp.get(v));
+        }
+        return ans;
+
+    }
+}
+// @lc code=end
+
