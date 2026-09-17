ea16d9fa38b1f3b4be5b0d2bea671ba55779b689
Liuqing Du <me@duliuqing.com>
2026-09-16 20:51:07 -0500

    1072

---
+++ 1072.按列翻转得到最大值等行数.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=1072 lang=javascript
+ *
+ * [1072] 按列翻转得到最大值等行数
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var maxEqualRowsAfterFlips = function(matrix) {
+    
+    let m = matrix.length, n = matrix[0].length;
+    let map = {};
+    for (let i = 0; i < m; i++) {
+        let arr = new Array(n).fill('0');
+        for (let j = 0; j < n; j++) {
+            // 如果 matrix[i][0] 为 1，则对该行元素进行翻转
+            arr[j] = '0' + (matrix[i][j] ^ matrix[i][0]);
+        }
+        let s = arr.join('');
+        map[s] = (map[s] || 0) + 1;
+    }
+    let res = 0;
+    for (let key in map) {
+        res = Math.max(res, map[key]);
+    }
+    return res;
+};
+// @lc code=end
+
