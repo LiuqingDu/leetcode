bf596efd69efbaa817879a2ae9acb74def85e6d8
Liuqing Du <me@duliuqing.com>
2026-09-20 22:38:48 -0500

    3314

---
+++ 3314.构造最小位运算数组-i.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=3314 lang=javascript
+ *
+ * [3314] 构造最小位运算数组 I
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var minBitwiseArray = function(nums) {
+    
+    for (let i = 0; i < nums.length; i++) {
+        let x = nums[i];
+        let res = -1;
+        let d = 1;
+        while ((x & d) !== 0) {
+            res = x - d;
+            d <<= 1;
+        }
+        nums[i] = res;
+    }
+    return nums;
+
+};
+// @lc code=end
+
