23c584f752c76b2627d308751d8b7f22c42d7841
Liuqing Du <me@duliuqing.com>
2026-09-24 21:14:25 -0500

    1248

---
+++ 1248.统计「优美子数组」.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=1248 lang=java
+ *
+ * [1248] 统计「优美子数组」
+ */
+
+// @lc code=start
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        
+        int n = nums.length;
+        int[] odd = new int[n + 2];
+        int ans = 0, cnt = 0;
+        for (int i = 0; i < n; ++i) {
+            if ((nums[i] & 1) != 0) {
+                odd[++cnt] = i;
+            }
+        }
+        odd[0] = -1;
+        odd[++cnt] = n;
+        for (int i = 1; i + k <= cnt; ++i) {
+            ans += (odd[i] - odd[i - 1]) * (odd[i + k] - odd[i + k - 1]); 
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
